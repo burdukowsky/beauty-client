4b28111fb2f1646f2471a016b31fb472b6a28d1c
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2018-06-05 22:33:36 +0700

    Implement simple template, add sidebar and routes
    - add main, about, sidebar, header, footer components
    - add two routes ('main' and 'about')
    - add ng-sidebar
    - reorganize application styles (add 'scss' folder)
    - change bootstrap styles importation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public opened = false;
+  public theBoundToggleSidebar: Function;
+
+  constructor(private router: Router) {
+    router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+      this.opened = false;
+    });
+  }
+
+  ngOnInit() {
+    this.theBoundToggleSidebar = this.toggleSidebar.bind(this);
+  }
+
+  public toggleSidebar() {
+    this.opened = !this.opened;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SidebarModule} from 'ng-sidebar';
+import {MainComponent} from './main/main.component';
+import {SidebarComponent} from './layout/sidebar/sidebar.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {FooterComponent} from './layout/footer/footer.component';
+import {AboutComponent} from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
