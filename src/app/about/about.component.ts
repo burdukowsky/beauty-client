de32d50c0709ecff4828501e5d2c9a01eb3f1786
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2018-06-28 22:05:08 +0700

    Fill 'about' page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faBuilding, faUsers, faUserFriends, faUserTie} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  public faBuilding = faBuilding;
+  public faUsers = faUsers;
+  public faUserFriends = faUserFriends;
+  public faUserTie = faUserTie;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
